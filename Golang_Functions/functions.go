e44a1eb88d5d1302bd903d41243503e3b1b2b88a
saifuddinsayed786 <saifuddin@nimapinfotech.com>
2021-08-28 12:13:56 +0530

    new commit

--- Golang_Functions/functions.go
+++ Golang_Functions/functions.go
@@ -9,17 +9,7 @@ func add(x int, y int) {
 	fmt.Println(total)
 }
 
-// Function with int as return type
-func addt(x int, y int) int {
-	total := 0
-	total = x + y
-	return total
-}
-
 func main() {
 	// Passing arguments
 	add(20, 30)
-	// Accepting return value in varaible
-	sum := addt(20, 30)
-	fmt.Println(sum)
 }
