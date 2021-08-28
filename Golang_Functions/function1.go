f599f479cfbb6d18921f94913a2fabafaea3827b
saifuddinsayed786 <saifuddin@nimapinfotech.com>
2021-08-28 12:12:55 +0530

    Function with return type

--- Golang_Functions/function1.go
+++ Golang_Functions/function1.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Function with int as return type
-func add(x int, y int) int {
+func addt(x int, y int) int {
 	total := 0
 	total = x + y
 	return total
@@ -11,6 +11,6 @@ func add(x int, y int) int {
 
 func main() {
 	// Accepting return value in varaible
-	sum := add(20, 30)
+	sum := addt(20, 30)
 	fmt.Println(sum)
 }
